5fa9f3a38897eec16099fe26b7eab39c34f02e56
Terry Fadz <terryfadz@Terrys-MacBook-Pro.local>
2019-03-30 11:56:41 -0500

    .gitignore is now working

---
+++ wp-content/themes/paradise/page-people.php
@@ -0,0 +1,47 @@
+<?php /* Template Name: People */ ?>
+
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+
+	<main id="main" class="site-main container">
+
+		<section class="row">
+          <div class="col col-12" style="padding-top: 5rem;">
+            <ul class="par-staff">
+
+              <?php 
+
+              // The Arguments
+              $args = array(
+                'post_type' => 'people',
+                'posts_per_page' => '-1',
+                'orderby' => 'date',
+                'order' => 'ASC'
+            );
+
+              // The Query
+              $query_people = new WP_Query($args);
+
+               // The Loop
+              if( $query_people->have_posts() ) : ?>
+
+
+                <?php while ( $query_people->have_posts() ) : $query_people->the_post(); ?>
+                   <li><a href="<?php the_permalink(); ?>">
+                      <h2><?php the_title() ?></h2>
+                      <figure><img src="<?php the_field('p_image') ?>" /></figure></a>
+
+                  </li>
+              <?php endwhile; ?> 
+          <?php endif; ?>
+
+      </ul>
+  </div>
+</section>
+
+</main><!-- #main -->
+</div>
+
+<?php
+
+get_footer();
