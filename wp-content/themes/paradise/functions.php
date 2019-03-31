c1b67747db29cd57de68b653ae250722c2a202e6
Terry Fadz <alaterry@gmail.com>
2019-03-31 10:07:25 -0500

    Added work custom post type

--- wp-content/themes/paradise/functions.php
+++ wp-content/themes/paradise/functions.php
@@ -206,6 +206,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 // Register Custom Post Type
+// People
 function custom_post_type() {
  
     $labels = array(
@@ -249,3 +250,47 @@ function custom_post_type() {
  
 // Hook into the 'init' action
 add_action( 'init', 'custom_post_type', 0 );
+
+
+// WORK
+function work_post_type() {
+ 
+    $labels = array(
+        'name'                => _x( 'Work', 'Post Type General Name', 'text_domain' ),
+        'singular_name'       => _x( 'Project', 'Post Type Singular Name', 'text_domain' ),
+        'menu_name'           => __( 'Work', 'text_domain' ),
+        'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+        'all_items'           => __( 'All Work', 'text_domain' ),
+        'view_item'           => __( 'View Item', 'text_domain' ),
+        'add_new_item'        => __( 'Add New Project', 'text_domain' ),
+        'featured_image'      => __( 'Featured Image', 'text_domain' ),
+        'add_new'             => __( 'Add New', 'text_domain' ),
+        'edit_item'           => __( 'Edit Item', 'text_domain' ),
+        'update_item'         => __( 'Update Item', 'text_domain' ),
+        'search_items'        => __( 'Search Item', 'text_domain' ),
+        'not_found'           => __( 'Not found', 'text_domain' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+    );
+    $args = array(
+        'label'               => __( 'Work', 'text_domain' ),
+        'labels'              => $labels,
+        'taxonomies'          => array( 'category', 'post_tag' ),
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons-format-video',
+        'can_export'          => true,
+        'has_archive'         => true,
+        'supports'            => array( 'title', 'editor', 'author', 'thumbnail' ),
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'page',
+    );
+    register_post_type( 'work', $args );
+ 
+}
+add_action( 'init', 'work_post_type', 0 );
